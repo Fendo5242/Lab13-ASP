dd5e23af61a80dd58067a45b1b5b61c73fbe1a91
Alumno <Alumno@LAB1504-09>
2024-06-15 12:49:24 -0500

    Agregar archivos de proyecto.

---
+++ Lab13/Models/StoreContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Lab13.Models
+{
+    public class StoreContext: DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<Detail> Details { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer("Server=LAB1504-09\\SQLEXPRESS;Database=market;User Id=Fendo;Password=123456;trustservercertificate=True");
+
+        }
+    }
+}
